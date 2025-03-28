93c8e067e44f9c3450e2989432b0c7c10d63fd9a
muhammad ans <muhammad.ans@asappstudio.com>
2025-03-28 15:17:15 +0500

    first commit

---
+++ utils/data/experience.js
@@ -0,0 +1,32 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Front End Engineer',
+    company: "Asapp Studio",
+    duration: "(Feb 2024 - Present)"
+  },
+  {
+    id: 3,
+    title: "Front End Developer",
+    company: "KTECHZ",
+    duration: "(Feb 2021 - Dec 2023)"
+  },
+  {
+    id: 2,
+    title: "Front End Developer",
+    company: "Arkaa Consutants",
+    duration: "(Jan 2021 - Nov 2021)"
+  },
+  {
+    id: 4,
+    title: "Wordpress Developer",
+    company: "WtechSols",
+    duration: "(Oct 2020 - Dec 2020)"
+  },
+  {
+    id: 4,
+    title: "Shopify Store Designer",
+    company: "Softgenni",
+    duration: "(May 2020 - Sep 2020)"
+  }
+]
---
+++ utils/data/personal-data.js
@@ -0,0 +1,11 @@
+export const personalData = {
+  name: "Muhammad Ans",
+  profile: '/profile.jpg',
+  designation: "Front-End Developer",
+  description: "My name is Muhammad Ans. I am a self-motivated IT professional with a strong foundation in problem-solving and a passion for continuous learning in web technologies. As a frontend developer, I have experience delivering high-quality, dynamic, and responsive websites for international clients. I am seeking a remote position where I can leverage my expertise in modern frontend development to contribute to a global team. I am open to job opportunities that align with my skills and my passion for building secure, efficient, and user-friendly web applications. I am open to job opportunities that align with my skills.",
+  email: 'muhammadans772@gmail.com',
+  address: 'Lahore, Pakistan ',
+  github: 'https://github.com/MuhammadAns1',
+  linkedIn: 'https://www.linkedin.com/in/muhammad-ans-231866240/',
+  resume: "https://drive.google.com/file/d/1mxtNnT3y8xHB_hR_Q9Ct6pupLX7dUaLv/view"
+}
